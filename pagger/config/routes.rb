4fcc19377bb122dc5a18a799ae4a45a76fadb7e8
charles <superwanderman@gmail.com>
2012-12-19 23:13:55 +0800

    add controller

--- pagger/config/routes.rb
+++ pagger/config/routes.rb
@@ -1,6 +1,8 @@
 Pagger::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  root :to => "home#index"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
