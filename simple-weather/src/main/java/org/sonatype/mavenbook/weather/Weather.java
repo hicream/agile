cf343d2941b0533394122eba2c8bab48b596fa5f
charleslei <superwanderman@gmail.com>
2013-05-29 00:37:21 +0800

    add

--- simple-weather/src/main/java/org/sonatype/mavenbook/weather/Weather.java
+++ simple-weather/src/main/java/org/sonatype/mavenbook/weather/Weather.java
@@ -1,6 +1,6 @@
 package org.sonatype.mavenbook.weather;
 
-public class Weather{
+public class Weather {
   private String city;
   private String region;
   private String country;
@@ -8,21 +8,20 @@ public class Weather{
   private String temp;
   private String chill;
   private String humidity;
-
-  public Weather(){}
-
-  public String getCity() {return city;}
-  public void setCity(String city) {this.city =city;}
-
-  public String getRegion() {return region;}
-  public void setRegion(String region) {this.region = region;}
-
-  public String getCountry() {return region;}
-  public void setRegion(String region) {this.region = region;}
-
-  public String getRegion() {return region;}
-  public void setRegion(String region) {this.region = region;}
-
-  public String getRegion() {return region;}
-  public void setRegion(String region) {this.region = region;}
+  public Weather() {}
+  public String getCity() { return city; }
+  public void setCity(String city) { this.city = city; }
+  public String getRegion() { return region; }
+  public void setRegion(String region) { this.region = region; }
+  public String getCountry() { return country; }
+  public void setCountry(String country) { this.country = country; }
+  public String getCondition() { return condition; }
+  public void setCondition(String condition) { this.condition = condition; }
+  public String getTemp() { return temp; }
+  public void setTemp(String temp) { this.temp = temp; }
+  public String getChill() { return chill; }
+  public void setChill(String chill) { this.chill = chill; }
+  public String getHumidity() { return humidity; }
+  public void setHumidity(String humidity) { this.humidity = humidity; }
 }
+
