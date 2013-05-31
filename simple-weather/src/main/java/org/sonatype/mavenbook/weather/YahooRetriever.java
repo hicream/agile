6a56f42262993067f19906fa4da2894b4a6a748c
charleslei <superwanderman@gmail.com>
2013-05-31 11:40:59 +0800

    add simple-weather

--- simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
+++ simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
@@ -1,11 +1,14 @@
 package org.sonatype.mavenbook.weather;
+
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+
 import org.apache.log4j.Logger;
 
 public class YahooRetriever {
   private static Logger log = Logger.getLogger(YahooRetriever.class);
+
   public InputStream retrieve(int zipcode) throws Exception {
     log.info( "Retrieving Weather Data" );
     String url = "http://weather.yahooapis.com/forecastrss?p=" + zipcode;
